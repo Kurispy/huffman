dbb3169b28c601aa8f0dd45584e51a12c6664e91
Chris <tech@ucdavis.edu>
2013-02-18 01:21:24 -0800

    Some methods implemented

--- Huffman.cpp
+++ Huffman.cpp
@@ -2,4 +2,56 @@
 
 using namespace std;
 
+void Huffman::readData() {
+    while (!cin.eof())
+        frequency_list_[cin.get()]++;
+    
+    //Reset get pointer to the beginning of the stream
+    cin.seekg(0);
+}
 
+void Huffman::createTree() {
+    HuffNode *huffnode;
+    QueueNode *queuenode;
+    
+    //Create the priority queue
+    for (int i = 0; i < 256; i++) {
+        if (frequency_list_[i] > 0) {
+            huffnode = new HuffNode(i, frequency_list_[i], NULL, NULL);
+            queuenode = new QueueNode(huffnode);
+            queue_.insert(queuenode);
+            queue_.size_++;
+        }
+    }
+    
+    //Combine nodes from the queue until a single tree is left
+    while (queue_.size_ > 1) {
+        huffnode = queue_.front()->huff;
+        queue_.deletefront();
+        if (huffnode < queue_.front()->huff)
+            huffnode = new HuffNode(findMinChar(huffnode, queue_.front()->huff), huffnode->weight + queue_.front()->huff->weight, huffnode, queue_.front()->huff);
+        else
+            huffnode = new HuffNode(findMinChar(huffnode, queue_.front()->huff), huffnode->weight + queue_.front()->huff->weight, queue_.front()->huff, huffnode);
+        
+        queue_.deletefront();
+        queuenode = new QueueNode(huffnode)
+        queue_.insert(queuenode);
+    }
+}
+
+void Huffman::encode() {
+    DFS(queue_.front()->huff, 0);
+}
+
+void Huffman::writeOut() {
+    //Magic cookie
+    cout << "HUFFMA3\0";
+    
+    //Frequencies
+    for (int i = 0; i < 256; i++) {
+        cout.write((char*) &frequency_list_[i], sizeof(frequency_list_[i]));
+    }
+    
+    //Data
+    //TODO: Bitwise endianness
+}
--- Huffman.h
+++ Huffman.h
@@ -5,24 +5,41 @@
 
 using namespace std;
 
-struct Node {
+struct HuffNode {
     int min_char;
     unsigned int weight;
-    Node *left;
-    Node *right;
+    HuffNode *left;
+    HuffNode *right;
+    bool operator<(HuffNode &rhs);
+};
+
+struct QueueNode {
+    HuffNode *huff;
+    QueueNode *left;
+    QueueNode *right;
+};
+
+class PriorityQueue {
+    QueueNode *front_;
+public:
+    int size_;
+    void insert(QueueNode *node);
+    void deletefront();
+    QueueNode* front();
 };
 
 class Huffman {
 private:
     unsigned int frequency_list_[256];
-    int code_table_[256]; //Store codes as ints maybe? Convert to string on print?
-    Node *char_queue_[256]; //Need to either make a queue or min heap
-    Node *inter_queue_[256];
+    int code_table_[256];
+    PriorityQueue queue_;
 public:
     void readData();
     void createTree();
     void encode();
     void writeOut();
+    int findMinChar();
+    void DFS(HuffNode *huffnode, int code);
 };
 
 #endif	/* HUFFMAN_H */
